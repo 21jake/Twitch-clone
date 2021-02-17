153c8ced69e74dcf901bee7cf50dfe0c78776415
21jake <jakethecat21@gmail.com>
2021-02-17 16:33:26 +0700

    UI - streaming video

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,21 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, createRef } from "react";
 import { useParams } from "react-router-dom";
 import { fetchStream } from "../../actions";
 import { connect } from "react-redux";
+import flv from "flv.js";
 
 const StreamShow = (props) => {
   const { id } = useParams();
   const { stream } = props;
+  const videoRef = createRef();
+  const videoPlayer = flv.createPlayer({
+    type: 'flv',
+    url: `http://localhost:8000/live/${id}.flv`
+  })
+
   useEffect(() => {
-    props.fetchStream(id)
-  }, [])
+    props.fetchStream(id);
+    videoPlayer.attachMediaElement(videoRef.current);
+    videoPlayer.load();
+    return function cleanUp() {
+      videoPlayer.destroy();
+    };
+  }, []);
+
   return (
-      !stream ?  "Loading" : 
-      <div>
-        <h1>{stream.title}</h1>
-        <h5>{stream.description}</h5>
-      </div>
-      
+    <div>
+      <video
+        ref={videoRef}
+        style={{ width: "100%" }}
+        controls
+      />
+      {!stream ? "Loading" :
+        <>
+          <h1>{stream.title}</h1>
+          <h5>{stream.description}</h5>
+        </>
+      }
+    </div>
+
   )
 }
 
